4f0bb955ee3164bbd556106477c46f7d70b3e742
Med-Li-Jr <medlawani.jr@gmail.com>
2022-07-29 00:41:02 +0100

    Init Done

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  allAnchor = document.getElementsByClassName("all-anchor");
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  setMenuActive(anchor : any, e:any){
+
+    
+    for (let i = 0; i < this.allAnchor.length; i++) {
+      this.allAnchor[i].classList.remove('my-navbar-active')
+      if(this.allAnchor[i].classList.contains(anchor)) 
+        this.allAnchor[i].classList.add("my-navbar-active")
+    }
+      
+    location.hash = "#" + anchor
+  }
+  
 }
